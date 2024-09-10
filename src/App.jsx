d4316e9c9a8e3b51964fbccc64bf97f0241a0980
juanpabl090 <juanrona12@gmail.com>
2024-09-09 19:54:11 -0600

    create & improve carpets organization, create first login test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import './App.css'
+import axios from 'axios';
+import './assets/styles/App.css'
+
+const handleLogin = async () => {
+  try {
+    const response = axios.get("http://localhost:8081/login", {
+      auth: {
+        username: 'admin',
+        password: '1234'
+      }
+    });
+    console.log((await response).data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <button type="button" onClick={handleLogin}>Iniciar sesion</button>
     </>
   )
 }
